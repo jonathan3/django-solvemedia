e8cf380acb4734b97cfd25748850a2306c340dd8
Jonathan Prior <newintentions@jonathanprior.name>
2013-05-19 17:13:31 +0200

    Force SSL in captcha widget

--- solvemedia/widgets.py
+++ solvemedia/widgets.py
@@ -6,7 +6,7 @@ from solvemedia import SolveMedia
 class SolveMediaCaptcha(forms.widgets.Widget):
     def render(self, name, value, attrs=None):
         sm = SolveMedia(settings.SM_CKEY, settings.SM_VKEY, settings.SM_HKEY)
-        return mark_safe(sm.get_html() + "<br>")
+        return mark_safe(sm.get_html(use_ssl=True) + "<br>")
 
     def value_from_datadict(self, data, files, name):
         return {'challenge': data.get('adcopy_challenge'), 'response': data.get('adcopy_response')}
