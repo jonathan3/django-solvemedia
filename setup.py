bfe38a9979b7c6a1a4534a8b33b32a777464fef2
Jonathan Prior <newintentions@jonathanprior.name>
2013-05-19 17:13:53 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='django-solvemedia',
-      version='0.0.1',
+      version='0.0.2',
       description='Django integration for Solve Media CAPTCHA/Type-in ads',
       author='whitehat2k9',
       url='https://github.com/whitehat2k9/django-solvemedia',
